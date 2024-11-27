04b722c2d4cd9fd924fdc7e1036ce61948e30dc9
Pierceid <128907409+Pierceid@users.noreply.github.com>
2024-11-27 02:40:07 +0100

    refactored

--- Classes/Strategy/RangeSearchStrategy.cs
+++ Classes/Strategy/RangeSearchStrategy.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace GeoGUI.Classes.Strategy {
     public class RangeSearchStrategy<T, U> : IStrategy<T, U> where T : Item where U : IKey<U> {
