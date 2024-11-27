3c4d8dc736d01a9554cc368a875b605edbc587fc
Pierceid <128907409+Pierceid@users.noreply.github.com>
2024-11-27 02:39:48 +0100

    updated

--- Classes/Strategy/RangeSearchStrategy.cs
+++ Classes/Strategy/RangeSearchStrategy.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System;
+using System.Collections.Generic;
 
 namespace GeoGUI.Classes.Strategy {
     public class RangeSearchStrategy<T, U> : IStrategy<T, U> where T : Item where U : IKey<U> {
@@ -10,7 +10,24 @@ namespace GeoGUI.Classes.Strategy {
         }
 
         public List<T> Traverse(KDTree<T, U> tree, U keys) {
-            return tree.GetAllNodes().Where(node => node.KeysData.CloseWithin(keys, range)).SelectMany(node => node.NodeData).ToList();
+            List<T> result = new List<T>();
+
+            if (tree.Root == null) return result;
+
+            Stack<Node<T, U>> nodesToVisit = new Stack<Node<T, U>>();
+            nodesToVisit.Push(tree.Root);
+
+            while (nodesToVisit.Count > 0) {
+                Node<T, U> currentNode = nodesToVisit.Pop();
+
+                if (currentNode.KeysData.CloseWithin(keys, this.range)) result.AddRange(currentNode.NodeData);
+
+                if (currentNode.LeftSon != null) nodesToVisit.Push(currentNode.LeftSon);
+
+                if (currentNode.RightSon != null) nodesToVisit.Push(currentNode.RightSon);
+            }
+
+            return result;
         }
     }
 }
